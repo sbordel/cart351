e3ce69e6ecb7c82d94dbcdbec107888ab6b9790d
sam_ <45014739+sbordel@users.noreply.github.com>
2021-11-14 22:00:51 -0500

    finalized conversational form with responsive question display

--- exercises/exercise_03/js/script.js
+++ exercises/exercise_03/js/script.js
@@ -6,11 +6,13 @@ $(document).ready(function () {
   let veggieBtn = $("#veggie button");
   let sauceBtn = $("#sauce button");
 
+  let veggieArray = [];
+  let veggieList;
 
   $("#modal-one").show();
   $('.icon-container').show();
   $("#modal-two").hide();
-
+  
   $(".one").delay(2000).fadeIn(200);
 
   $("#chat-btn").click(function () {
@@ -21,15 +23,15 @@ $(document).ready(function () {
   $("#order-btn").click(function () {
     $("#modal-two").show();
   });
-
+  // when pizza size is chosen, display next message
   sizeBtn.click(function (){
     if (sizeBtn == true);{
       $(".size").delay(1000).fadeIn(200);
       $(".size").text("I would like a " + $(this).attr('value') + " pizza pls");
       $(".two").delay(4000).fadeIn(200);
-
     }
   });
+    // when pizza shape is chosen, display next message
   shapeBtn.click(function (){
     if (shapeBtn == true);{
       $(".shape").delay(1000).fadeIn(200);
@@ -37,6 +39,7 @@ $(document).ready(function () {
       $(".three").delay(4000).fadeIn(200);
     }
   });
+  // when cheese amount is chosen, display next message
   cheeseBtn.click(function (){
     if (cheeseBtn == true);{
       $(".cheese").delay(1000).fadeIn(200);
@@ -44,8 +47,37 @@ $(document).ready(function () {
       $(".four").delay(4000).fadeIn(200);
     }
   });
-});
+  //when veggie buttons are selected, push its corresponding values in an array called "veggieArray"
+  veggieBtn.click(function (){
+      if (veggieBtn.pressed);{
+      veggieArray.push($(this).attr('value'));
 
+      if (veggieArray.length == 2){
+        veggieList = veggieArray.join(" and ");
+      }
+      else {
+        veggieList = veggieArray.join(", ");
+      };
+      console.log(veggieList);
+      };
+  // when user clicks the submit button, the array is then displayed as a list of color toppings, then the next question is displayed
+  $("#submit").click(function (){
+        $(".veggie").text(veggieList + " veggies");
+        console.log(veggieList);
+        $(".veggie").delay(1000).fadeIn(200);
+        $(".veggie").text($(this).attr('value'));
+        $(".five").delay(4000).fadeIn(200);
+      });
+    });
+  // when sauce button is selected, end conversation
+  sauceBtn.click(function (){
+    if (sauceBtn == true);{
+      $(".sauce").delay(1000).fadeIn(200);
+      $(".sauce").text($(this).attr('value'));
+      $(".six").delay(4000).fadeIn(200);
+    }
+  });
+});
 
 //close modals when clicking the "x" button
 function closeOrder() {
