0201482646ba1f929d7d9b02fffa6fe4aed7faf0
sam_ <45014739+sbordel@users.noreply.github.com>
2021-12-04 17:47:25 -0500

    added loop function for garden buttons

--- project/P4-final_project/js/script.js
+++ project/P4-final_project/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // INDEX
     let modalOne = $("#one.about-modal");
     let modalTwo = $("#two.about-modal");
     let modalThree = $("#three.about-modal");
@@ -11,32 +12,79 @@ $(document).ready(function () {
     $("#a.nav-button").click(function () {
         modalOne.show();
     });
-
     $("#b.next-button").click(function () {
         modalOne.hide();
         modalTwo.show();
     });
-
     $("#c.next-button").click(function () {
         modalTwo.hide();
         modalThree.show();
     });
-
     $("#close-modal").click(function () {
         modalThree.hide();
     });
 
-    $("#gd-one").hover(function () {
-        $("#tp-one").show();
+
+    //  GARDEN
+    let btnContainer = $("#tb-footer");
+
+    const btnName = ["⇮", "plant", "fertilize", "water", "liquid sunshine"];
+    const btnInfo = ["hover on the buttons above", "upload an image to the server and expand the garden", "supply the garden with nutrients", "rehydrate the garden", "provide the garden with light to help it stay bright and healthy"];
+    let btnNum = btnName.length;
+    //make corresponding name + description appear if hovered, and remain if clicked
+
+    $(".garden-button").mouseenter(function () {
+        for (let i = 0; i < btnNum; i++) {
+            if ($(this).attr('id') == "plant-btn") {
+                $("h1").text(btnName[1]);
+                $("p").text(btnInfo[1]);
+            }
+            else if ($(this).attr('id') == "fertilize-btn") {
+                $("h1").text(btnName[2]);
+                $("p").text(btnInfo[2]);
+            }
+            else if ($(this).attr('id') == "water-btn") {
+                $("h1").text(btnName[3]);
+                $("p").text(btnInfo[3]);
+            }
+            else if ($(this).attr('id') == "sun-btn") {
+                $("h1").text(btnName[4]);
+                $("p").text(btnInfo[4]);
+            }
+            else {
+                $("h1").text("");
+                $("p").text("");
+            };
+        };
     });
-    $("#gd-two").hover(function () {
-        $("#tp-two").show();
-    });
-    $("#gd-three").hover(function () {
-        $("#tp-three").show();
-    });
-    $("#gd-four").hover(function () {
-        $("#tp-four").show();
+    $(".garden-button").mouseleave(function () {
+        $("h1").text("");
+        $("p").text("");
     });
 
+    $(".garden-button").click(function () {
+        $(".garden-button").off("mouseleave");
+
+        for (let i = 0; i < btnNum; i++) {
+            if ($(this).attr('id') == "plant-btn") {
+                $("h1").text(btnName[1]);
+                $("p").text(btnInfo[1]);
+            }
+            else if ($(this).attr('id') == "fertilize-btn") {
+                $("h1").text(btnName[2]);
+                $("p").text(btnInfo[2]);
+            }
+            else if ($(this).attr('id') == "water-btn") {
+                $("h1").text(btnName[3]);
+                $("p").text(btnInfo[3]);
+            }
+            else if ($(this).attr('id') == "sun-btn") {
+                $("h1").text(btnName[4]);
+                $("p").text(btnInfo[4]);
+            }
+            else {
+                $(".garden-button").mouseleave();
+            };
+        };
+    });
 });
