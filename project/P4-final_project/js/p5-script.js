5ee4e281cd2242b4ea4d91473f906b9deaf6fe45
sam_ <45014739+sbordel@users.noreply.github.com>
2021-12-05 23:30:25 -0500

    created separate script file for p5js

---
+++ project/P4-final_project/js/p5-script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let img;
+
+function setup() {    
+let winW = windowWidth;
+let winH = windowHeight;
+
+let gardenCnv = createElement('div');
+//createCanvas(winW, winH);
+gardenCnv.id('garden-canvas');
+
+let modalCnv = createCanvas(300, 300);
+modalCnv.parent('img-upload');
+modalCnv.id('inline-upload');
+background(200);
+fill(0, 0, 255);
+textAlign(CENTER);
+textSize(30);
+text('⤒', width / 2, height / 2);
+modalCnv.drop(gotFile);
+};
+
+function draw() {
+if (img) {
+    image(img, 0, 0, width, height);
+}
+};
+
+function gotFile(file) {
+img = createImg(file.data, '').hide();
+}
--- project/P4-final_project/js/script.js
+++ project/P4-final_project/js/script.js
@@ -116,16 +116,5 @@ $(document).on({
             gardenModal.hide();
         });
     });
-
-    let imgElement = document.getElementById('imageSrc');
-    let inputElement = document.getElementById('fileInput');
-    inputElement.addEventListener('change', (e) => {
-    imgElement.src = URL.createObjectURL(e.target.files[0]);
-    }, false);
-    imgElement.onload = function() {
-    let mat = cv.imread(imgElement);
-    cv.imshow('canvasOutput', mat);
-    mat.delete();
-    };
-    
 });
+
