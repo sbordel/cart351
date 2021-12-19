a767b5265482f0cd246752fd57f732651f258b32
sam_ <45014739+sbordel@users.noreply.github.com>
2021-12-18 21:57:35 -0500

    cleaning up files + creating separate script file for index modals

--- project/P4-final_project/js/script.js
+++ project/P4-final_project/js/script.js
@@ -1,35 +1,4 @@
 $(document).ready(function () {
-
-    aboutModals();
-
-    function aboutModals() {
-        // INDEX
-
-        //about modal functions
-        let modalOne = $("#one.about-modal");
-        let modalTwo = $("#two.about-modal");
-        let modalThree = $("#three.about-modal");
-
-        modalOne.hide();
-        modalTwo.hide();
-        modalThree.hide();
-
-        $("#a.nav-button").click(function () {
-            modalOne.show();
-        });
-        $("#b.next-button").click(function () {
-            modalOne.hide();
-            modalTwo.show();
-        });
-        $("#c.next-button").click(function () {
-            modalTwo.hide();
-            modalThree.show();
-        });
-        $("#close-modal").click(function () {
-            modalThree.hide();
-        });
-    };
-
     let sunFilters;
 
     //get date, then month 
